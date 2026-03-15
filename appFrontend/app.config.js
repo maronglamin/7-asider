f3d89ffde8af1483fcdf32bd97f2910c4e9d424d
Modou Lamin Marong <mlmarong14036@gmail.com>
2026-03-15 01:33:59 +0000

    changes made while new shift to windows PC

--- appFrontend/app.config.js
+++ appFrontend/app.config.js
@@ -1,11 +1,14 @@
 // Dynamic Expo config to inject env vars into Constants.expoConfig.extra
 require('dotenv').config();
 
+const productionApiBase = 'https://seven-aside.phantommetrics.gm';
+const defaultApiBase = process.env.NODE_ENV === 'production' ? productionApiBase : 'http://localhost:4000';
+
 module.exports = ({ config }) => ({
   ...config,
   extra: {
     ...(config.extra || {}),
-    API_BASE: process.env.API_BASE || 'http://localhost:4000',
+    API_BASE: process.env.API_BASE || defaultApiBase,
   },
   plugins: [
     ...(config.plugins || []),
